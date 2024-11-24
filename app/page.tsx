55fc959ddaa4747a3e757a5f98248e1be32ec5fb
willy4202 <willy4202@naver.com>
2024-11-24 18:12:40 +0900

    primary 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className=" w-full h-full text-4xl ">
-      <div>홈</div>
+      <div className=" text-primary text-4xl  font-bold">홈</div>
     </main>
   );
 }
